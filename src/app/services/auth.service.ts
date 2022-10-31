c22905ba68cacbb694422b86a3cca515cdaacdf5
Alan <alanmar08@hotmail.com.ar>
2022-10-31 06:45:30 -0300

    servicios token y autorizacion

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { JwtDto } from '../models/jwt-dto';
+import { LoginUsuario } from '../models/login-usuario';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  URL = environment.URL + 'auth/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
+    return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario)
+  }
+}
